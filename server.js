59df526e3ad0dad3bbb667e69f34d95dab641105
Bryndye <57522753+Bryndye@users.noreply.github.com>
2026-01-16 12:12:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Listening", PORT));
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: allowedOrigin,
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -24,7 +28,7 @@ io.on("connection", (socket) => {
 
     // Pour envoyer un message à tous les clients
     io.emit("message", `serveur: ${msg}`);
-    
+
     // Pour envoyer un message à tous les clients sauf le client qui a envoyé le message
     // socket.broadcast.emit("message", `serveur: ${msg}`);
   });
@@ -34,6 +38,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("http://localhost:3000");
 });
