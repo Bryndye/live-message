573564f98b4314c52178b3ca30279be225162522
Bryndye <57522753+Bryndye@users.noreply.github.com>
2026-01-16 13:18:15 +0100

    Refactor socket events to use enums

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-
+const { Events } = require("./enums");
 const app = express();
 const server = http.createServer(app);
-const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
 const PORT = process.env.PORT || 3000;
 
+// anti xss
+
 const io = new Server(server);
+// const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
 // {
 //   cors: {
 //     origin: allowedOrigin,
@@ -20,20 +22,21 @@ app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
   console.log("client connecté", socket.id);
+  io.emit(Events.USER_CONNECTED, `serveur: ${socket.id} connecté`);
 
-  socket.on("message", (msg) => {
+  socket.on(Events.MESSAGE, (msg) => {
     console.log("reçu:", msg);
     // Pour envoyer un message au client qui a envoyé le message
-    // socket.emit("message", `serveur: ${msg}`);
+    // socket.emit(Events.MESSAGE, `serveur: ${msg}`);
 
     // Pour envoyer un message à tous les clients
-    io.emit("message", `serveur: ${msg}`);
+    io.emit(Events.MESSAGE, `serveur: ${msg}`);
 
     // Pour envoyer un message à tous les clients sauf le client qui a envoyé le message
-    // socket.broadcast.emit("message", `serveur: ${msg}`);
+    // socket.broadcast.emit(Events.MESSAGE, `serveur: ${msg}`);
   });
 
-  socket.on("disconnect", () => {
+  socket.on(Events.USER_DISCONNECTED, () => {
     console.log("client déconnecté");
   });
 });
