1aa944a07d0b14e123f43990639fce3564c6a6e5
Bryndye <57522753+Bryndye@users.noreply.github.com>
2026-01-16 16:37:10 +0100

    Add USER_ID event to Events enumeration

--- enums.js
+++ enums.js
@@ -1,7 +1,9 @@
 const Events = {
+    CONNECT: "connect",
     USER_CONNECTED: "user:connected",
     USER_DISCONNECTED: "user:disconnected",
     MESSAGE: "message",
+    USER_ID: "user:id",
 };
 
 module.exports = { Events };
