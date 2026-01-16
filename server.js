8f726538f4bcf5b6f7f52b245e423904183edf37
Bryndye <57522753+Bryndye@users.noreply.github.com>
2026-01-16 16:34:50 +0100

    Refactor socket.io event handling and user management

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const { Events } = require("./enums");
+const User = require("./user");
+
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
@@ -9,35 +11,60 @@ const PORT = process.env.PORT || 3000;
 // anti xss
 
 const io = new Server(server);
-// const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
-// {
-//   cors: {
-//     origin: allowedOrigin,
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   },
-// }
+const users = new Map();
 
 app.use(express.static(__dirname));
 
-io.on("connection", (socket) => {
+// Pour envoyer un message à tous les clients
+function sendMessageToAll(socket, message) {
+  console.log("user name:", users);
+  console.log("user name:", users.get(socket.id));
+  console.log("user name:", users.get(socket.id).name);
+  const userName = users.get(socket.id).name;
+  io.emit(Events.MESSAGE, userName + ": " + message);
+}
+
+// ASTUCES
+// Pour envoyer un message à tous les clients sauf le client qui a envoyé le message
+// socket.broadcast.emit(Events.MESSAGE, `serveur: ${msg}`);
+
+// Pour envoyer un message au client qui a envoyé le message
+// socket.emit(Events.MESSAGE, `serveur: ${msg}`);
+
+function escapeHtml(str) {
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+// const safe = escapeHtml(userInput);
+
+io.on(Events.CONNECT, (socket) => {
   console.log("client connecté", socket.id);
-  io.emit(Events.USER_CONNECTED, `serveur: ${socket.id} connecté`);
 
   socket.on(Events.MESSAGE, (msg) => {
     console.log("reçu:", msg);
-    // Pour envoyer un message au client qui a envoyé le message
-    // socket.emit(Events.MESSAGE, `serveur: ${msg}`);
 
-    // Pour envoyer un message à tous les clients
-    io.emit(Events.MESSAGE, `serveur: ${msg}`);
-
-    // Pour envoyer un message à tous les clients sauf le client qui a envoyé le message
-    // socket.broadcast.emit(Events.MESSAGE, `serveur: ${msg}`);
+    sendMessageToAll(socket, msg);
   });
 
-  socket.on(Events.USER_DISCONNECTED, () => {
-    console.log("client déconnecté");
+  socket.on(Events.USER_DISCONNECTED, (user=null) => {
+    console.log("client déconnecté", socket.id);
+    if (user !== null) {
+      socket.broadcast.emit(Events.USER_DISCONNECTED, `${user.name} vient de se déconnecter !`);
+    }
+    else {
+      socket.broadcast.emit(Events.USER_DISCONNECTED, `${users.get(socket.id).name} vient de se déconnecter !`);
+    }
+    users.delete(socket.id);
+  });
+
+  socket.on(Events.USER_ID, (user) => {
+    console.log("user id:", user);
+    users.set(socket.id, user);
+    socket.broadcast.emit(Events.USER_CONNECTED, `${user.name} vient de se connecter !`);
   });
 });
 
